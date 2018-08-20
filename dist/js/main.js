267b8aa64ae70d38f64b45e6889cdeabe6297188
Nianxu Wang <nianxuwa@usc.edu>
2018-08-20 10:05:29 -0700

    starting javascript controls

---
+++ dist/js/main.js
@@ -0,0 +1,6 @@
+// Select DOM items of the menu
+
+// selects the button-menu class (querySelector works with classes, ids...etc.)
+const menuButton = document.querySelector(".button-menu");
+const menuButton = document.querySelector(".button-menu");
+const menuButton = document.querySelector(".button-menu");
